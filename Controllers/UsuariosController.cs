2b4a80553586803fa174eeddcf11c4260b15ea61
KennyVell <kenneth.geme1@gmail.com>
2024-04-03 21:33:08 -0500

    crear o create

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using UsuariosProductos.Data;
+using UsuariosProductos.Models;
 namespace UsuariosProductos.Controllers;
 
 public class UsuariosController : Controller {
@@ -14,6 +15,20 @@ public class UsuariosController : Controller {
 
     public async Task<IActionResult> Detalles(int id) {
         return View(await _context.Usuarios.FirstOrDefaultAsync(m => m.Id == id));
+    }
+
+    public IActionResult Crear(){
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Crear(Usuario u){
+        //Agregar el usuario al DbSet
+        _context.Usuarios.Add(u);
+        //Guardamos los cambios realizados en el contexto de la base de datos
+        _context.SaveChanges();
+        //Nos redirigimos a otra vista
+        return RedirectToAction("Index");
     }
 
     public async Task<IActionResult> Eliminar(int id) {
